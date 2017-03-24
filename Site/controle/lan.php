dc5b019e3927bb727c3080e8e1a6b7fe2326b1a6
Yoeung <michelyoeung@hotmail.fr>
2017-03-24 14:58:55 +0100

    Fonctionnalite reception notification

--- Site/controle/lan.php
+++ Site/controle/lan.php
@@ -22,11 +22,33 @@
     foreach($tournoisLan as $element){
       $jeu = getJeu($element['IdJeu']);
       require("vue/lan/afficherTournoi.tpl");
-
     }
-
   }
 
+  function afficherCheckNotif($idlan){
+	require("modele/estinscrit.php");
+	$listeInscriptions = array();
+	$listeInscriptions = getEstInscrit();
+	$existeDeja = false;
+	foreach($listeInscriptions as $inscription){
+		if ($inscription['IdUtilisateur'] == $_SESSION['profil']['IdUtilisateur'] && $inscription['IdLan'] == $idlan){
+			$existeDeja = true;
+		}
+	}
+    if(isset ($_SESSION['profil']['Pseudo']) && !$existeDeja){
+      require("vue/lan/afficherCheck.tpl");
+    }
+  }
 
+  function recevoirNotif(){
+    require("modele/estinscrit.php");
+    if($_POST['notif'] == "ok"){
+      setEstInscrit($_SESSION['profil']['IdUtilisateur'], $_GET['param']);
+	  header('Location: index.php?controle=start&action=accueil');
+    }
+	else {
+		header('Location: index.php?controle=lan&action=afficherLan&param=' . $_GET['param']);
+	}
+  }
 
 ?>
